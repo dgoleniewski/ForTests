eb0108dd9e01e7b54204fdce7a483ffdda4be1c2
dgoleniewski <goleniewski.damian1@gmail.com>
2020-05-31 21:05:35 +0200

    Init

---
+++ app/src/test/java/com/e/fortests/ExampleUnitTest.java
@@ -0,0 +1,53 @@
+package com.e.fortests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.edu.pl"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"));
+    }
+
+    @Test
+    public void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""));
+    }
+
+    @Test
+    public void emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null));
+    }
+}
